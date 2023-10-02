c2339df5daddd778fdffeb12da248765fc8270ac
Pawel Krol <paullkroll@icloud.com>
2023-10-02 15:11:54 +0200

    Add h1 title in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <h1>Change title for task</h1>
       Hello World !!!!
     </div>
   );
